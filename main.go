fa2a5dfaee2e1722bc4b5fb61db64afb3b13d9ea
Myrat <zjsegzhf@gmail.com>
2021-07-15 23:03:35 +0800

    test: trigger

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "hello, push")
+		fmt.Fprint(w, "hello, xx")
 	})
 
 	port := os.Getenv("PORT")
